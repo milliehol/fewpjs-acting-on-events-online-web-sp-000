9d44ec6b2e9f5290b51ad42ec39b39a6b37d7669
Carla Holcomb <carla.programmer@gmail.com>
2020-05-29 23:49:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
     let dodger = document.getElementById("dodger");
 
         document.addEventListener("keydown", function(e) {
-      console.log(e.key);
+       if (e.key === "ArrowLeft") {
+    moveDodgerLeft(); }
+    else {
+      moveDodgerRight();
+    }
+  }
     });
     
         function moveDodgerLeft() {
