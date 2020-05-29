9a9ee54c8e2b528ff1885d130478c5848eaed230
Carla Holcomb <carla.programmer@gmail.com>
2020-05-29 23:51:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 
     let dodger = document.getElementById("dodger");
 
-        document.addEventListener("keydown", function(e) {
-       if (e.key === "ArrowLeft") {
-    moveDodgerLeft(); }
-    else {
-      moveDodgerRight();
-    }
+    document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
   }
-    });
-    
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
         function moveDodgerLeft() {
       let leftNumbers = dodger.style.left.replace("px", "");
       let left = parseInt(leftNumbers, 10);
