aa1af604400192ee202b49c318fd7c6ba3d23b0b
Carla Holcomb <carla.programmer@gmail.com>
2020-05-29 23:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@
         document.addEventListener("keydown", function(e) {
       console.log(e.key);
     });
+    
+        function moveDodgerLeft() {
+      let leftNumbers = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumbers, 10);
+     
+      if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+      }
+    }
