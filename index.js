7b4e61aaf9e6d11a1488beb017de76a52b81d0af
Carla Holcomb <carla.programmer@gmail.com>
2020-05-29 23:50:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@
       let right = parseInt(rightNumbers, 10);
      
       if (right > 0) {
-        dodger.style.right = `${right + 1}px`;
+        dodger.style.right = `${left + 1}px`;
       }
     }
